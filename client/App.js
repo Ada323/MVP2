073a68c0cbc16fe14ef8dfd3bb9dfb159d3ff1b8
Ada323 <mag242@gmail.com>
2017-06-13 11:11:49 -0500

    Initial transpiling working, need to tweak implementation of external API call

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-// import Search from './search'
+import React, { Component } from 'react';
+import Search from './search'
 
 class App extends Component {
   constructor(props) {
@@ -7,35 +7,58 @@ class App extends Component {
 
     this.state = {
     aqi: '',
-    searchValue: ''
+    searchValue: '',
+    message: '',
   }
 };
 
-  // handleSearchChange = (e) => {
-  //   e.preventDefault();
-  //   const value = e.target.value;
-  //
-  //   this.setState({
-  //     searchValue: value,
-  //   });
-  //
-  //   if (value === '') {
-  //     this.setState({
-  //       aqi: ''
-  //     });
-  //   } else {
-  //     Search.search(value, (aqi) => {
-  //       this.setState({
-  //         aqi: 'TEST',
-  //       });
-  //     });
-  //   }
+  handleSearchChange(e) {
+    e.preventDefault();
+    const value = e.target.value;
 
+    this.setState({
+      searchValue: value,
+    });
 
-// Place these in input field
-  // value={this.state.searchValue}
-  // onChange={this.handleSearchChange}
+    if (value === '') {
+      this.setState({
+        aqi: ''
+      });
+    } else {
+      Search.search(value, (aqi) => {
+        if (aqi) {
+          if (aqi > 0 && aqi <= 50) {
+            this.setState({
+              message: 'Good: Air quality is considered satisfactory, and air pollution poses little or no risk.',
+            })
+          } else if (aqi > 50 && aqi <= 100 ) {
+            this.setState({
+              message: 'Moderate: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.',
+            })
 
+          } else if (aqi > 100 && aqi <= 150) {
+            this.setState({
+              message: 'Unhealthy for Sensitive Groups: Members of sensitive groups may experience health effects. The general public is not likely to be affected.',
+            })
+          } else if (aqi > 150 && aqi <= 200) {
+            this.setState({
+              message: 'Unhealthy: Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.',
+            })
+          } else if (aqi > 200 && aqi <= 300) {
+            this.setState({
+              message: 'Very Unhealthy: Health warnings of emergency conditions. The entire population is more likely to be affected.',
+            })
+          } else {
+            this.setState({
+              message: 'Hazardous: Health warnings of emergency conditions. The entire population is more likely to be affected.',
+            })
+          }
+          this.setState({
+            aqi: `Current air quality: ${aqi}`,
+          });
+        }
+      });
+    }
   }
 
   render() {
@@ -44,20 +67,20 @@ class App extends Component {
         <div className="App-header">
           <h2>SMOG REPORT</h2>
         </div>
-          <p className="App-intro">
-          Find out if your air is safe in realtime
-          </p>
         <div className="search-bar form-inline">
-          <input
-          className="form-control"
-          type="text"
-          placeholder="Search by City"
-          />
+        <input
+        className="form-control"
+        type="text"
+        placeholder="Search by City"
+        value={this.state.searchValue}
+        onChange={this.handleSearchChange}
+        />
         </div>
         <h3 className="aqi-result">{this.state.aqi}</h3>
+        <p className="message">{this.state.message}</p>
       </div>
     );
   }
-
+}
 
 export default App;
--- client/search.js
+++ client/search.js
@@ -1,28 +1,11 @@
-//THIS FETCH REQUEST SHOULD CALL THE EXTERNAL API DIRECTLY
+import $ from 'jquery';
 
-// 
-// function search(query) {
-//   console.log('QUERY', query);
-//   return fetch(`http://api.waqi.info/feed/${query}/?token=f0be4f42542748763ca5795f6e5768ba0303a091`, {
-//     accept: 'application/json',
-//   }).then(checkStatus)
-//     .then(parseJSON)
-// }
-//
-// function checkStatus(response) {
-//   if (response.status >= 200 && response.status < 300) {
-//     return response;
-//   }
-//   const error = new Error(`HTTP Error ${response.statusText}`);
-//   error.status = response.statusText;
-//   error.response = response;
-//   console.log(error);
-//   throw error;
-// }
-//
-// function parseJSON(response) {
-//   return response.json();
-// }
-//
-// const Search = { search };
-// export default Search;
+const search = function(query, callback) {
+  $.get(`http://api.waqi.info/feed/${query}/?token=f0be4f42542748763ca5795f6e5768ba0303a091`, function(result) {
+    callback(result.data.aqi)
+  })
+};
+
+
+const Search = { search };
+export default Search;
