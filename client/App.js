ba9b49cf9851fa9150b7405b369c3c5774d7c5f5
Ada323 <mag242@gmail.com>
2017-06-12 22:53:19 -0500

    Second attempt without create react app using browserify

---
+++ client/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import '../style.css';
+// import Search from './search'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    aqi: '',
+    searchValue: ''
+  }
+};
+
+  // handleSearchChange = (e) => {
+  //   e.preventDefault();
+  //   const value = e.target.value;
+  //
+  //   this.setState({
+  //     searchValue: value,
+  //   });
+  //
+  //   if (value === '') {
+  //     this.setState({
+  //       aqi: ''
+  //     });
+  //   } else {
+  //     Search.search(value, (aqi) => {
+  //       this.setState({
+  //         aqi: 'TEST',
+  //       });
+  //     });
+  //   }
+
+
+// Place these in input field
+  // value={this.state.searchValue}
+  // onChange={this.handleSearchChange}
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>SMOG REPORT</h2>
+        </div>
+        <p className="App-intro">
+        Find out if your air is safe in realtime
+        </p>
+        <div className="search-bar form-inline">
+        <input
+        className="form-control"
+        type="text"
+        placeholder="Search by City"
+        />
+        </div>
+        <h3 className="aqi-result">{this.state.aqi}</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,30 @@
+
+var express = require('express');
+var browserify = require('browserify-middleware');
+var morgan = require('morgan');
+var path = require('path');
+var config = require('./config.js');
+
+var app = express();
+
+app.use(morgan('dev'));
+
+
+app.get('/bundle.js', browserify('./client/index.js', {
+  transform: [ [ require('babelify'), { presets: ['es2015', 'react'] } ] ]
+}));
+
+app.use(express.static(path.join(__dirname, './client')));
+
+
+app.use('/style.css', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'style.css'));
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('404 - Page Not Found');
+});
+
+app.listen(4000, function() {
+  console.log('Listening on port 4000');
+});
